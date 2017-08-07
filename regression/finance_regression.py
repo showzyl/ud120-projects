61a910407ff46fc380f13cd549069bdc361b83be
showzyl <showzyl520@gmail.com>
2017-08-07 11:54:38 +0800

    add regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+# features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -38,11 +39,12 @@ test_color = "r"
 from sklearn import linear_model
 reg = linear_model.LinearRegression()
 
-reg.fit(feature_test, target_test)
+reg.fit(feature_train, target_train)
 
 print 'slope: ', reg.coef_
 print 'intercept: ', reg.intercept_
-print 'r-squared score: ', reg.score(feature_test, target_test)
+print 'r-squared score: ', reg.score(feature_train, target_train)
+print 'test r-squared score: ', reg.score(feature_test, target_test)
 
 
 
@@ -59,13 +61,19 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 
 
-
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print 'New Slope:',reg.coef_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+
